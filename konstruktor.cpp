4afd935d367033a610e0fffbb161328f1afdd5f7
safiradwikhairunisa <safiradwikhairunisa@gmail.com>
2025-05-17 14:29:23 +0700

    menambah class mahasiswa public

--- konstruktor.cpp
+++ konstruktor.cpp
@@ -7,4 +7,11 @@ class mahasiswa
     private:
     int nim;
     string nama;
-}
+
+    public:
+    mahasiswa();
+    mahasiswa(int);
+    mahasiswa(string);
+    mahasiswa(int iNim, string iNama);
+    void cetak();
+};
