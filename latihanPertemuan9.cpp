f2c6d58fe2f336a912ba122afc52831ac23339e5
safiradwikhairunisa <safiradwikhairunisa@gmail.com>
2025-05-17 16:20:07 +0700

    class dosen

--- latihanPertemuan9.cpp
+++ latihanPertemuan9.cpp
@@ -2,12 +2,31 @@
 #include <string>
 using namespace std;
 
+class dosen;
+class staff;
+class universitas;
+
 class mahasiswa
 {
-    public:
+    private:
     string nama;
     int nim;
+    float nilai; //dapat diubah dengan dosen
 
+    public:
+    mahasiswa(string nama, int nim):nama(nama), nim(nim), nilai(0){}
+    
+    friend class dosen; //agar dosen bisa mengakses mahasiswa
+};
+
+class Dosen
+{
     private:
-    float nilai;
+    string nama;
+    int nidn;
+    string pangkat;
+    float gaji;
+
+    public:
+    Dosen(string nama, int nidn):nama(nama), nidn(nidn), pangkat("Asisten ahli"), gaji(0){}
 };
