a2a422699c04e0b0eaf9dc9aa22f51e564a03eaf
safiradwikhairunisa <safiradwikhairunisa@gmail.com>
2025-05-17 14:24:15 +0700

    menambah unt main

--- destruktor.cpp
+++ destruktor.cpp
@@ -39,3 +39,11 @@ void angka::isiData()
     }
     cout << endl;
 }
+
+int main()
+{
+    angka belajarcpp(3); //constructor dipanggil
+    angka *ptrBelajarcpp = new angka(5);  //constructor dipanggil
+    delete ptrBelajarcpp; //desctrucor dipanggil
+    return 0;
+} //destruction dipanggil
