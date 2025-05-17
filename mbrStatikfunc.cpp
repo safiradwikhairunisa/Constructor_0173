a88a6df12003a06c8dfdccf2d98902cd105d6134
safiradwikhairunisa <safiradwikhairunisa@gmail.com>
2025-05-17 14:52:19 +0700

    menambah class public

--- mbrStatikfunc.cpp
+++ mbrStatikfunc.cpp
@@ -6,4 +6,16 @@ class mahasiswa
 {
     private:
     static int nim;
-}
+
+    public:
+    int id;
+    string nama;
+
+    void setID();
+    void printAll();
+
+    static void setNim(int pNim) {nim=pNim; /*definisi function*/}
+    static int getNim() {return nim; /*definisi function*/}
+
+    mahasiswa(string pnama):nama(pnama){setID();}
+};
